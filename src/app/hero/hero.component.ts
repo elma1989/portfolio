a7a000dd59c7ce78450430df7029fecafd4bd842
Marco Elste <marco.elste@web.de>
2025-09-12 16:26:15 +0200

    chore: Added component header

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
 @Component({
   selector: 'section[app-hero]',
-  imports: [],
+  imports: [
+    HeaderComponent,
+  ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
