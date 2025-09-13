0430667e1b21719bb12dc7685212175b7ca67ac6
Marco Elste <marco.elste@web.de>
2025-09-13 18:40:01 +0200

    chore: Added component mobOberlay

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
+import { MobOverlayComponent } from './mob-overlay/mob-overlay.component';
 
 @Component({
   selector: 'section[app-hero]',
   imports: [
     HeaderComponent,
+    MobOverlayComponent
   ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
---
+++ src/app/hero/mob-overlay/mob-overlay.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mob-overlay',
+  imports: [],
+  templateUrl: './mob-overlay.component.html',
+  styleUrl: './mob-overlay.component.scss'
+})
+export class MobOverlayComponent {
+
+}
