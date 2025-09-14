b78c135e64228d88a5ce419bd4ab6e3317aacfc5
Marco Elste <marco.elste@web.de>
2025-09-14 09:31:22 +0200

    feat: Updated mob-overlay.component.html: Added Nav-Links

--- src/app/hero/mob-overlay/mob-overlay.component.ts
+++ src/app/hero/mob-overlay/mob-overlay.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { NavService } from '../../shared/services/nav.service';
+import { CommonModule } from '@angular/common';
+import { LangSelComponent } from '../../shared/components/lang-sel/lang-sel.component';
 
 @Component({
   selector: 'app-mob-overlay',
   imports: [
     HeaderComponent,
+    CommonModule,
+    LangSelComponent
   ],
   templateUrl: './mob-overlay.component.html',
   styleUrl: './mob-overlay.component.scss'
