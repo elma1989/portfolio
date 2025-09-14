6c282cac8f3629e2129002d0c1f657597e0d47ff
Marco Elste <marco.elste@web.de>
2025-09-14 12:42:10 +0200

    feat: Updated social-media.component.html: Full content

--- src/app/hero/mob-overlay/mob-overlay.component.ts
+++ src/app/hero/mob-overlay/mob-overlay.component.ts
@@ -3,13 +3,15 @@ import { HeaderComponent } from '../../shared/components/header/header.component
 import { NavService } from '../../shared/services/nav.service';
 import { CommonModule } from '@angular/common';
 import { LangSelComponent } from '../../shared/components/lang-sel/lang-sel.component';
+import { SocialMediaComponent } from '../social-media/social-media.component';
 
 @Component({
   selector: 'app-mob-overlay',
   imports: [
     HeaderComponent,
     CommonModule,
-    LangSelComponent
+    LangSelComponent,
+    SocialMediaComponent
   ],
   templateUrl: './mob-overlay.component.html',
   styleUrl: './mob-overlay.component.scss'
--- src/app/hero/social-media/social-media.component.ts
+++ src/app/hero/social-media/social-media.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-social-media',
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './social-media.component.html',
   styleUrl: './social-media.component.scss'
 })
 export class SocialMediaComponent {
-
+  public socialMedias: {name: string, url:string, img:string}[] = [
+    {name:'linkedin', url: '', img: 'linkedin.svg'},
+    {name: 'github', url: 'https://github.com/elma1989', img: 'github.svg'},
+    {name: 'mail', url: 'mailto:marco.elste@web.de', img: 'mail.svg'}
+  ]
 }
