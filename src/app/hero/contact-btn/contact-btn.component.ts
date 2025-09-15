5434b0d68d5fd90f1cb24f86e0ac8fe7fc72d2a2
Marco Elste <marco.elste@web.de>
2025-09-15 21:44:42 +0200

    feat: Updated component contactBtn - full content

--- src/app/hero/contact-btn/contact-btn.component.ts
+++ src/app/hero/contact-btn/contact-btn.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-btn',
-  imports: [],
+  imports: [
+    TranslatePipe,
+  ],
   templateUrl: './contact-btn.component.html',
   styleUrl: './contact-btn.component.scss'
 })
