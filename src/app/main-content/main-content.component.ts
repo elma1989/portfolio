4049d5605b204248e62c65d16dfdfa44b8b27bd7
Marco Elste <marco.elste@web.de>
2025-09-16 07:33:44 +0200

    chore: Added section component about

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-main-content',
   imports: [
     HeroComponent,
+    AboutComponent
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
