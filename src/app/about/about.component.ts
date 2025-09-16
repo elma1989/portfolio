ef8766a559f8da222b98c50d1be14f0b7abbd45c
Marco Elste <marco.elste@web.de>
2025-09-16 11:20:37 +0200

    feat: Updated about.component.html: Content About decription

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'section[app-about]',
-  imports: [],
+  imports: [
+    TranslatePipe,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
