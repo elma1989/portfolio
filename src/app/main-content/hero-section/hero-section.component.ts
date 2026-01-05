3da8075bb6ee73bda668fd1066af84273fb67a03
Marco Elste <marco.elste@web.de>
2026-01-05 20:47:38 +0100

    chore: Added HeroSectionComponent

---
+++ src/app/main-content/hero-section/hero-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'section[hero]',
+  imports: [],
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.css'
+})
+export class HeroSectionComponent {
+
+}
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,13 @@ import { Component, computed, HostListener, inject, Signal } from '@angular/core
 import { SectionService } from '../shared/services/section.service';
 import { CommonModule } from '@angular/common';
 import { SectionType } from '../shared/enums/section-type';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-main-content',
   imports: [
-    CommonModule
+    CommonModule,
+    HeroSectionComponent
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
@@ -15,6 +17,7 @@ export class MainContentComponent {
   private sec: SectionService = inject(SectionService);
   protected mobile: Signal<boolean> = computed(() => this.sec.mobile());
   protected section: Signal<SectionType> = computed(() => this.sec.section());
+  protected SectionType = SectionType;
 
   @HostListener('window:resize')
   onResize() {
