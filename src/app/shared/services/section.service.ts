bf1f4d24b7ecf1ac37534b42e6f8ddefdcc03223
Marco Elste <marco.elste@web.de>
2026-01-05 13:11:31 +0100

    chore: Added SectionService

---
+++ src/app/shared/services/section.service.ts
@@ -0,0 +1,27 @@
+import { HostListener, Injectable, Signal, signal, WritableSignal } from '@angular/core';
+import { SectionType } from '../enums/section-type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SectionService {
+  private _mobile: WritableSignal<boolean> = signal<boolean>(false);
+  private _section: WritableSignal<SectionType> = signal<SectionType>(SectionType.HERO);
+
+  get mobile(): Signal<boolean> { return this._mobile.asReadonly() }
+
+  set mobile(state: boolean) { this._mobile.set(state) }
+
+  get section(): Signal<SectionType> { return this._section.asReadonly() }
+
+  set section(section: SectionType) { this._section.set(section) }
+
+  isMobile(): boolean {
+    return false;
+  }
+
+  @HostListener('window.resize')
+  onResize() {
+    
+  }
+}
