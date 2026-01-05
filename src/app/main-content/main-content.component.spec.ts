f4491ba25aa289ca06110266c4063baece95e614
Marco Elste <marco.elste@web.de>
2026-01-05 18:11:19 +0100

    Update main-content.component.spec.ts - Added tests for Skills-Section

--- src/app/main-content/main-content.component.spec.ts
+++ src/app/main-content/main-content.component.spec.ts
@@ -137,4 +137,50 @@ describe('MainContentComponent', () => {
       expect(about()?.classList.contains('bg-cwhite') ?? false).toBeTrue();
     });
   });
+
+  describe('Skills', () => {
+    const skills: () => HTMLElement | null =
+      () => element.querySelector('section[skills]')
+
+    beforeEach(() => {
+      sec.section = SectionType.ABOUT;
+      fixture.detectChanges();
+    });
+
+    it('should exist on mobile', () => {
+      (window as any).innerWidth = 672;
+      window.dispatchEvent(new Event('resize'));
+      fixture.detectChanges();
+      expect(skills()).toBeTruthy();
+    });
+
+    it('should exist on desktop', () => {
+      (window as any).innerWidth = 1024;
+      window.dispatchEvent(new Event('resize'));
+      fixture.detectChanges();
+      expect(skills()).toBeTruthy();
+    });
+
+    it('should have full viewport size', () => {
+      expect(skills()?.classList.contains('h-dvh') ?? false).toBeTrue();
+      expect(skills()?.classList.contains('w-full') ?? false).toBeTrue();
+    });
+
+    it('should padding top 9dvh on mobile', () => {
+      expect(skills()?.classList.contains('pt-[9dvh]') ?? false).toBeTrue();
+    });
+
+    it('should padding top 10dvh on desktop', () => {
+      expect(skills()?.classList.contains('lg:pt-[10dvh]') ?? false).toBeTrue();
+    });
+
+    it('should content x-center', () => {
+      expect(skills()?.classList.contains('flex') ?? false).toBeTrue();
+      expect(skills()?.classList.contains('justify-center') ?? false).toBeTrue();
+    });
+
+    it('should have black background', () => {
+      expect(skills()?.classList.contains('bg-cwhite') ?? false).toBeTrue();
+    });
+  });
 });
