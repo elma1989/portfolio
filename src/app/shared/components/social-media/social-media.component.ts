4758207bfa287da6dce8d10ef1066ea5baca974c
Marco Elste <marco.elste@web.de>
2026-01-07 16:52:59 +0100

    chore: Updated social-media.component.ts - Insert social-media data

--- src/app/shared/components/social-media/social-media.component.ts
+++ src/app/shared/components/social-media/social-media.component.ts
@@ -1,4 +1,12 @@
-import { Component, input, InputSignal } from '@angular/core';
+import { Component, input, InputSignal, signal, WritableSignal } from '@angular/core';
+
+type Media =  {
+  link: string,
+  light: string,
+  lightHover: string,
+  dark: string,
+  darkHover: string
+}
 
 @Component({
   selector: 'social-media',
@@ -8,4 +16,29 @@ import { Component, input, InputSignal } from '@angular/core';
 })
 export class SocialMediaComponent {
   dark: InputSignal<boolean> = input<boolean>(false);
+  gitHover: WritableSignal<boolean> = signal<boolean>(false);
+  linkedinHover: WritableSignal<boolean> = signal<boolean>(false);
+  mailHover: WritableSignal<boolean> = signal<boolean>(false);
+
+  medias: Media[] = [
+    {
+      link: 'https://github.com/elma1989/',
+      light: 'github-light.svg',
+      lightHover: 'github-light-hover.svg',
+      dark: 'github-dark.svg',
+      darkHover: 'github-dark-hover.svg'
+    }, {
+      link: 'linkedin.com',
+      light: 'linkedin-light.svg',
+      lightHover: 'linkedin-light-hover.svg',
+      dark: 'linkedin-dark.svg',
+      darkHover: 'linkedin-dark-hover.svg'
+    }, {
+      link: 'mailto: marco.elste@web.de',
+      light: 'mail-light.svg',
+      lightHover: 'mail-light-hover.svg',
+      dark: 'mail-dark.svg',
+      darkHover: 'mail-dark-hover.svg'
+    }
+  ]
 }
