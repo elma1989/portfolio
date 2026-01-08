dc00b71ef583f198c8cb40246892b069a6c0eba0
Marco Elste <marco.elste@web.de>
2026-01-08 13:16:55 +0100

    feat: Updated menu-overlay.component.ts - navigate()

--- src/app/shared/components/menu-overlay/menu-overlay.component.ts
+++ src/app/shared/components/menu-overlay/menu-overlay.component.ts
@@ -1,15 +1,64 @@
-import { Component, output, OutputEmitterRef } from '@angular/core';
+import { Component, inject, output, OutputEmitterRef } from '@angular/core';
+import { TranslatePipe } from '../../pipes/translate.pipe';
+import { SectionType } from '../../enums/section-type';
+import { SectionService } from '../../services/section.service';
+import { Router } from '@angular/router';
+
+type NavItem = {
+  transId: string,
+  section: SectionType,
+  navId: string
+}
 
 @Component({
   selector: 'menu-overlay',
-  imports: [],
+  imports: [
+    TranslatePipe
+  ],
   templateUrl: './menu-overlay.component.html',
   styleUrl: './menu-overlay.component.css'
 })
 export class MenuOverlayComponent {
   closed: OutputEmitterRef<void> = output();
+  sec: SectionService = inject(SectionService);
+  router: Router = inject(Router);
+  items: NavItem[] = [
+    {
+      transId: 'menu.about',
+      section: SectionType.ABOUT,
+      navId: 'about'
+    }, {
+      transId: 'menu.skill',
+      section: SectionType.SKILLS,
+      navId: 'skills'
+    }, {
+      transId: 'menu.project',
+      section: SectionType.PROJECTS,
+      navId: 'projects'
+    }, {
+      transId: 'menu.reference',
+      section: SectionType.REFERENCES,
+      navId: 'references'
+    }, {
+      transId: 'menu.contact',
+      section: SectionType.CONTACT,
+      navId: 'contact'
+    }
+  ]
 
+  /** Closes the menu. */
   close(): void {
     this.closed.emit();
+  }
+
+  /**
+   * Navigates to Section.
+   * @param index Indes of NavItem
+   */
+  navigate(index: number) {
+    if (index < 5) {
+      this.sec.section = this.items[index].section;
+      this.router.navigate([], { fragment: this.items[index].navId });
+    }
   }
 }
