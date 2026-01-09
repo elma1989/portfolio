e2dd0433f094cbf62019f0c9461dc925512e599c
Marco Elste <marco.elste@web.de>
2026-01-09 10:40:46 +0100

    test: Update hero-section.component.spec.ts - Create tests for content-area

--- src/app/main-content/hero-section/hero-section.component.spec.ts
+++ src/app/main-content/hero-section/hero-section.component.spec.ts
@@ -5,6 +5,7 @@ import { HeroSectionComponent } from './hero-section.component';
 describe('HeroSectionComponent', () => {
   let component: HeroSectionComponent;
   let fixture: ComponentFixture<HeroSectionComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,55 @@ describe('HeroSectionComponent', () => {
 
     fixture = TestBed.createComponent(HeroSectionComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Content', () => {
+    const content: () => HTMLDivElement | null =
+      () => element.querySelector('.content');
+    
+    it('should have content', () => {
+      expect(content()).toBeTruthy();
+    });
+
+    it('should be a flex-box', () => {
+      expect(content()?.classList.contains('flex') ?? false).toBeTrue();
+    });
+  });
+
+  describe('Content-Area', () => {
+    const area: () => HTMLDivElement | null =
+      () => element.querySelector('.content>.content-area');
+
+      it('should have a content-area', () => {
+        expect(area()).toBeTruthy();
+      });
+
+      it('should have full size on mobile', () => {
+        expect(area()?.classList.contains('w-full') ?? false).toBeTrue();
+        expect(area()?.classList.contains('h-full') ?? false).toBeTrue();
+      });
+
+      it('should have width 90% on desktop', () => {
+        expect(area()?.classList.contains('w-9/10') ?? false).toBeTrue();
+      });
+
+      it('should have position relative', () => {
+        expect(area()?.classList.contains('relative') ?? false).toBeTrue();
+      });
+
+      it('should have direction column for moble', () => {
+        expect(area()?.classList.contains('flex') ?? false).toBeTrue();
+        expect(area()?.classList.contains('flex-col') ?? false).toBeTrue();
+      });
+
+      it('should have direction row on desktop', () => {
+        expect(area()?.classList.contains('lg:flex-row') ?? false).toBeTrue();
+      })
+  })
 });
