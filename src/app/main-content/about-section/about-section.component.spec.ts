1fbe5c8ff37a30cf929799b5f40afbc35ce02fe5
Marco Elste <marco.elste@web.de>
2026-01-12 15:30:25 +0100

    test: Update about-section.component.spec.ts - Create tests for content

--- src/app/main-content/about-section/about-section.component.spec.ts
+++ src/app/main-content/about-section/about-section.component.spec.ts
@@ -5,6 +5,7 @@ import { AboutSectionComponent } from './about-section.component';
 describe('AboutSectionComponent', () => {
   let component: AboutSectionComponent;
   let fixture: ComponentFixture<AboutSectionComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,28 @@ describe('AboutSectionComponent', () => {
 
     fixture = TestBed.createComponent(AboutSectionComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Content', () => {
+    const content: () => HTMLDivElement | null =
+      () => element.querySelector('.content');
+
+    it('should have content limiter', () => {
+      expect(content()).toBeTruthy();
+    });
+
+    it('shhould be flex-box', () => {
+      const elem: HTMLElement | null = content();
+      if (elem) {
+        expect(elem.classList).toContain('flex');
+      }
+      expect(elem).toBeTruthy();
+    });
   });
 });
