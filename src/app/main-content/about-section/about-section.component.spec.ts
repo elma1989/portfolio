f57dd46e8afc60869995eb3d518df5d75c2eac3b
Marco Elste <marco.elste@web.de>
2026-01-12 15:57:21 +0100

    test: Update about-section.component.spec.ts - Create tests for content-area

--- src/app/main-content/about-section/about-section.component.spec.ts
+++ src/app/main-content/about-section/about-section.component.spec.ts
@@ -39,4 +39,62 @@ describe('AboutSectionComponent', () => {
       expect(elem).toBeTruthy();
     });
   });
+
+  describe('content-area', () => {
+    const area: () => HTMLDivElement | null =
+      () => element.querySelector('.content>.contentarea');
+    
+    it('should have content-area', () => {
+      expect(area()).toBeTruthy();
+    });
+
+    it('should have full height', () => {
+      const elem: HTMLDivElement | null = area();
+      if (elem) {
+        expect(elem.classList).toContain('h-full');
+      }
+      expect(elem).toBeTruthy();
+    });
+
+    it('should have full width on mobile', () => {
+      const elem: HTMLDivElement | null = area();
+      if (elem) {
+        expect(elem.classList).toContain('w-full');
+      }
+      expect(elem).toBeTruthy();
+    });
+
+    it('should have 90% width on desktop', () => {
+      const elem: HTMLDivElement | null = area();
+      if (elem) {
+        expect(elem.classList).toContain('lg:h-9/10');
+      }
+      expect(elem).toBeTruthy();
+    });
+
+    it('should have position relative', () => {
+      const elem: HTMLDivElement | null = area();
+      if (elem) {
+        expect(elem.classList).toContain('relative');
+      }
+      expect(elem).toBeTruthy();
+    });
+
+    it('should have padding 1rem on mobile', () => {
+      const elem: HTMLDivElement | null = area();
+      if (elem) {
+        expect(elem.classList).toContain('p-4');
+      }
+      expect(elem).toBeTruthy();
+    });
+
+    it('should have padding 4.5 7 4.5 7 on desktop', () => {
+      const elem: HTMLDivElement | null = area();
+      if (elem) {
+        expect(elem.classList).toContain('px-28');
+        expect(elem.classList).toContain('py-18');
+      }
+      expect(elem).toBeTruthy();
+    });
+  });
 });
