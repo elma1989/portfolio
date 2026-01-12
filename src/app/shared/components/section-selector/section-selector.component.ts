bc336c948046e97b1d8247a40b5efb829fa122a5
Marco Elste <marco.elste@web.de>
2026-01-12 12:03:05 +0100

    feat: Updated section-selector.ts - hasDarkTheme(), getContent(), getColorClass(), changeSection()

--- src/app/shared/components/section-selector/section-selector.component.ts
+++ src/app/shared/components/section-selector/section-selector.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, input, InputSignal } from '@angular/core';
 import { SectionService } from '../../services/section.service';
+import { SectionType } from '../../enums/section-type';
 
 @Component({
   selector: 'section-selector',
@@ -10,4 +11,50 @@ import { SectionService } from '../../services/section.service';
 export class SectionSelectorComponent {
   index: InputSignal<number> = input<number>(0);
   sec: SectionService = inject(SectionService);
+  selectors: SectionType[] = [
+    SectionType.HERO,
+    SectionType.ABOUT,
+    SectionType.SKILLS,
+    SectionType.PROJECTS,
+    SectionType.REFERENCES,
+    SectionType.CONTACT
+  ]
+
+  // #region Methods
+  /**
+   * Checks. if section has dark theme.
+   * @returns True, if section has dark theme.
+   */
+  private hasDarkTheme(): boolean {
+    const section = this.sec.section();
+    return section == SectionType.ABOUT || section == SectionType.PROJECTS;
+  }
+
+  /**
+   * Gets section-symbol from index.
+   * @param index - Index of section.
+   * @returns - Content-Symbol for section.
+   */
+  protected getContent(index: number): string {
+    return this.index() == index ? '◆' : '●';
+  }
+
+  /**
+   * Gets CSS-Color-Class form index of section.
+   * @param index - Index of section.
+   * @returns - CSS Class for txt-color.
+   */
+  protected getColorClass(index: number): string {
+    if (this.index() == index) return 'tx-orange'
+    return this.hasDarkTheme() ? 'tx-black' : 'tx-white'
+  }
+
+  /**
+   * Changes section on click on button.
+   * @param index - Index of Section.
+   */
+  protected changeSection(index: number): void {
+    if(index >= 0 && index < 6) this.sec.section = this.selectors[index];
+  }
+  // #endregion
 }
