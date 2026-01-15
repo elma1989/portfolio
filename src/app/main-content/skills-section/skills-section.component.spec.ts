3f0d4bab9cc32275a57d040ae80221aa10c8eb94
Marco Elste <marco.elste@web.de>
2026-01-15 15:28:34 +0100

    chore: Updated skills-sction.component.spec.ts - Create tests for content-area

--- src/app/main-content/skills-section/skills-section.component.spec.ts
+++ src/app/main-content/skills-section/skills-section.component.spec.ts
@@ -5,6 +5,7 @@ import { SkillsSectionComponent } from './skills-section.component';
 describe('SkillsSectionComponent', () => {
   let component: SkillsSectionComponent;
   let fixture: ComponentFixture<SkillsSectionComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,63 @@ describe('SkillsSectionComponent', () => {
 
     fixture = TestBed.createComponent(SkillsSectionComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Content', () => {
+    const content: () => HTMLDivElement | null =
+      () => element.querySelector('.content');
+
+    it('should have content-limiter', () => {
+      expect(content()).toBeTruthy();
+    });
+
+    it('should be flex-box', () => {
+      expect(content()?.classList).toContain('flex')
+    });
+  });
+
+  describe('Content-Area', () => {
+    const area: () => HTMLDivElement | null =
+      () => element.querySelector('.content-area');
+
+    it('should have Conent-Area', () => {
+      expect(area()).toBeTruthy();
+    });
+
+    it('should have full height', () => {
+      expect(area()?.classList).toContain('h-full');
+    });
+
+    it('should have full width on mobile', () => {
+      expect(area()?.classList).toContain('w-full');
+    });
+
+    it('should have 90% width on desktop', () => {
+      expect(area()?.classList).toContain('lg:w-9/10');
+    });
+
+    it('should have padding 1rem on mobile', () => {
+      expect(area()?.classList).toContain('p-4');
+    });
+
+    it('should have padding 10 0 10 7 on desktop', () => {
+      const elem: HTMLElement | null = area();
+      expect(elem?.classList).toContain('py-40');
+      expect(elem?.classList).toContain('pr-0');
+      expect(elem?.classList).toContain('pl-28');
+    });
+
+    it('should have gap 2rem col', () => {
+      const elem: HTMLElement | null = area();
+      expect(elem?.classList).toContain('flex');
+      expect(elem?.classList).toContain('flex-col');
+      expect(elem?.classList).toContain('gap-8');
+    });
   });
 });
