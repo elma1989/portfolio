7d5e2c0dd947af6b1f53fe1b3e39b4f84edd3c87
Marco Elste <marco.elste@web.de>
2026-01-15 08:22:21 +0100

    test: Update about-overlay.spec.ts - Create tests for header

--- src/app/main-content/about-section/about-overlay/about-overlay.component.spec.ts
+++ src/app/main-content/about-section/about-overlay/about-overlay.component.spec.ts
@@ -5,6 +5,7 @@ import { AboutOverlayComponent } from './about-overlay.component';
 describe('AboutOverlayComponent', () => {
   let component: AboutOverlayComponent;
   let fixture: ComponentFixture<AboutOverlayComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,35 @@ describe('AboutOverlayComponent', () => {
 
     fixture = TestBed.createComponent(AboutOverlayComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Header', () => {
+    const header: () => HTMLElement | null =
+      () => element.querySelector('header');
+
+    it('should have header', () => {
+      expect(header()).toBeTruthy();
+    });
+
+    it('should have full width', () => {
+      expect(header()?.classList).toContain('w-full');
+    });
+
+    it('should be space between on mobile', () => {
+      const elem: HTMLElement | null = header();
+      expect(elem?.classList).toContain('flex');
+      expect(elem?.classList).toContain('justify-between');
+      expect(elem?.classList).toContain('items-center');
+    });
+
+    it('should be center on desktp', () => {
+      expect(header()?.classList).toContain('lg:justify-center');
+    });
+  })
 });
