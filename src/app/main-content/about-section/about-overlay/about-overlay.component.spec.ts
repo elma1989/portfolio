cd3f3eb853bc67923623c247e665176743b389b2
Marco Elste <marco.elste@web.de>
2026-01-15 08:52:46 +0100

    test: Upadate about-overlay.spec.ts - Create tests for h2, close-btn

--- src/app/main-content/about-section/about-overlay/about-overlay.component.spec.ts
+++ src/app/main-content/about-section/about-overlay/about-overlay.component.spec.ts
@@ -1,21 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AboutOverlayComponent } from './about-overlay.component';
+import { Signal, signal } from '@angular/core';
+import { TranslationService } from '../../../shared/services/translation.service';
+import { SectionService } from '../../../shared/services/section.service';
+
+const langSignal = signal<'en' | 'de'>('en');
+
+const translationServiceMock = {
+  get lang(): Signal<'en' | 'de'> { return langSignal.asReadonly(); },
+  set lang(lang: 'en' | 'de') { langSignal.set(lang); },
+  translate: (key: string) => `translated: ${key}`
+};
 
 describe('AboutOverlayComponent', () => {
   let component: AboutOverlayComponent;
   let fixture: ComponentFixture<AboutOverlayComponent>;
   let element: HTMLElement;
+  let sec: SectionService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutOverlayComponent]
+      imports: [AboutOverlayComponent],
+      providers: [
+        {
+          provide: TranslationService,
+          useValue: translationServiceMock
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AboutOverlayComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
+    sec = TestBed.inject(SectionService);
     fixture.detectChanges();
   });
 
@@ -45,5 +63,66 @@ describe('AboutOverlayComponent', () => {
     it('should be center on desktp', () => {
       expect(header()?.classList).toContain('lg:justify-center');
     });
-  })
+
+    it('should have bold text', () => {
+      expect(header()?.classList).toContain('text-bold')
+    });
+  });
+
+  describe('H2', () => {
+    const h2: () => HTMLElement | null =
+      () => element.querySelector('header>h2');
+
+    it('should have h2', () => {
+      expect(h2()).toBeTruthy();
+    });
+
+    it('should have size 1.5rem', () => {
+      expect(h2()?.classList).toContain('text-[1.5rem]/[1.5rem]');
+    });
+
+    it('should have content "about.overlay-title', () => {
+      expect(h2()?.textContent ?? '')
+        .toBe('translated: about.overlay-title');
+    });
+  });
+
+  describe('Close btn', () => {
+    const close: () => HTMLButtonElement | null =
+      () => element.querySelector('header>button');
+    
+    it('should not render on desktop', () => {
+      sec.mobile = false;
+      fixture.detectChanges();
+      expect(close()).toBeNull();
+    });
+
+    describe('Mobile', () => {
+      beforeEach(() => {
+        sec.mobile = true;
+        fixture.detectChanges();
+      });
+
+      it('should reder on mobile', () => {
+        expect(close()).toBeTruthy();
+      });
+
+      it('should have size 2rem', () => {
+        const elem: HTMLButtonElement | null = close();
+        expect(elem?.classList).toContain('w-8');
+        expect(elem?.classList).toContain('h-8');
+      });
+
+      it('should have content center', () => {
+        const elem: HTMLButtonElement | null = close();
+        expect(elem?.classList).toContain('flex');
+        expect(elem?.classList).toContain('justify-center');
+        expect(elem?.classList).toContain('items-center');
+      });
+
+      it('should have content "X"', () => {
+        expect(close()?.textContent ?? '').toBe('X');
+      });
+    });
+  });
 });
