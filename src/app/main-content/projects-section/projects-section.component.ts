e9bd4b2421c990b26ca0994fcc0471921e0ee32a
Marco Elste <marco.elste@web.de>
2026-01-21 18:51:03 +0100

    chore: Updated projects.component.ts - Added projects

--- src/app/main-content/projects-section/projects-section.component.ts
+++ src/app/main-content/projects-section/projects-section.component.ts
@@ -1,5 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { TranslatePipe } from '../../shared/pipes/translate.pipe';
+
+type Project = {
+  name: string,
+  icon: string,
+  background: string,
+  flower: string,
+  screenschot: string,
+  skills: string[],
+  github: string,
+  url: string
+}
 
 @Component({
   selector: 'section[projects]',
@@ -10,5 +21,26 @@ import { TranslatePipe } from '../../shared/pipes/translate.pipe';
   styleUrl: './projects-section.component.css'
 })
 export class ProjectsSectionComponent {
-
+  protected currentProject: WritableSignal<number> = signal(0);
+  protected projects: Project[] = [
+    {
+      name: 'Join',
+      icon: 'check',
+      background: 'yellow',
+      flower: 'orange',
+      screenschot: 'join',
+      skills: ['HTML', 'CSS', 'TypeScript', 'Angular', 'Firebase'],
+      github: 'https://github.com/elma1989/join',
+      url: 'https://marco-elste.developerakademie.net/angular-projects/join/'
+    }, {
+      name: 'El Pollo Loco',
+      icon: 'ckicken',
+      background: 'orange',
+      flower: 'yellow',
+      screenschot: 'join',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      github: 'https://github.com/elma1989/join',
+      url: 'https://marco-elste.developerakademie.net/el-pollo-loco/'
+    }
+  ]
 }
