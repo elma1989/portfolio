5c24cbbfe5cc50afd737d284085748533f13cd16
Marco Elste <marco.elste@web.de>
2026-01-21 09:03:50 +0100

    chore: Updated ProjectsCompoent - Added content-area

--- src/app/main-content/projects-section/projects-section.component.spec.ts
+++ src/app/main-content/projects-section/projects-section.component.spec.ts
@@ -5,6 +5,7 @@ import { ProjectsSectionComponent } from './projects-section.component';
 describe('ProjectsSectionComponent', () => {
   let component: ProjectsSectionComponent;
   let fixture: ComponentFixture<ProjectsSectionComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,61 @@ describe('ProjectsSectionComponent', () => {
 
     fixture = TestBed.createComponent(ProjectsSectionComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Content', () => {
+    const content: () => HTMLDivElement | null =
+      () => element.querySelector('.content');
+
+    it('should have conent-limiter', () => {
+      expect(content()).toBeTruthy();
+    });
+
+    it('should be Flex-box', () => {
+      expect(content()?.classList).toContain('flex');
+    });
+  });
+
+  describe('Content-Area', () => {
+    const area: () => HTMLDivElement | null =
+      () => element.querySelector('.content>.content-area');
+
+    it('should have Content-Area', () => {
+      expect(area()).toBeTruthy();
+    });
+
+    it('should have full height', () => {
+      expect(area()?.classList).toContain('h-full');
+    });
+
+    it('should have full width on mobile', () => {
+      expect(area()?.classList).toContain('w-full');
+    });
+
+    it('should have 90% width on mobile', () => {
+      expect(area()?.classList).toContain('lg:w-9/10');
+    });
+
+    it('should have padding 1rem on mobile', () => {
+      expect(area()?.classList).toContain('p-4');
+    });
+
+    it('should have padding 2 0 2 7 on desktop', () => {
+      expect(area()?.classList).toContain('lg:py-8');
+      expect(area()?.classList).toContain('lg:pl-28');
+      expect(area()?.classList).toContain('lg:pr-0');
+    });
+
+    it('should have gap 2rem column', () => {
+      expect(area()?.classList).toContain('flex');
+      expect(area()?.classList).toContain('flex-col');
+      expect(area()?.classList).toContain('gap-8');
+    });
   });
 });
