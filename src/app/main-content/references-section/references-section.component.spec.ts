ee400a8c88bf4207e7b2a0722713a582dd392eb8
Marco Elste <marco.elste@web.de>
2026-01-27 09:40:28 +0100

    chore: Updated ReferencesComponent - Insert content area

--- src/app/main-content/references-section/references-section.component.spec.ts
+++ src/app/main-content/references-section/references-section.component.spec.ts
@@ -1,23 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ReferencesSectionComponent } from './references-section.component';
+import { Signal, signal } from '@angular/core';
+import { TranslationService } from '../../shared/services/translation.service';
+
+const langSignal = signal<'en' | 'de'>('en');
+
+const translationServiceMock = {
+  get lang(): Signal<'en' | 'de'> { return langSignal.asReadonly(); },
+  set lang(lang: 'en' | 'de') { langSignal.set(lang); },
+  translate: (key: string) => `translated: ${key}`
+};
 
 describe('ReferencesSectionComponent', () => {
   let component: ReferencesSectionComponent;
   let fixture: ComponentFixture<ReferencesSectionComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReferencesSectionComponent]
+      imports: [ReferencesSectionComponent],
+      providers: [
+        {
+          provide: TranslationService,
+          useValue: translationServiceMock
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ReferencesSectionComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Content', () => {
+    const content: () => HTMLDivElement | null =
+      () => element.querySelector('.content');
+
+    it('should have content-limiter', () => {
+      expect(content()).toBeTruthy();
+    });
+
+    it('should be a flex-box', () => {
+      expect(content()?.classList).toContain('flex')
+    });
+  });
+
+  describe('Content-Area', () => {
+    const area: () => HTMLDivElement | null =
+      () => element.querySelector('.content>.content-area');
+
+    it('should have content area', () => {
+      expect(area()).toBeTruthy();
+    });
+
+    it('should have full height', () => {
+      expect(area()?.classList).toContain('h-full');
+    });
+
+    it('should have full width on mobile', () => {
+      expect(area()?.classList).toContain('w-full');
+    });
+
+    it('should have 90% width on desktop', () => {
+      expect(area()?.classList).toContain('lg:w-9/10');
+    });
+
+    it('should have padding 1rem on mobile', () => {
+      expect(area()?.classList).toContain('p-4');
+    });
+
+    it('should have padding 2 0 2 7 on desktop', () => {
+      const ar = area();
+      expect(ar?.classList).toContain('lg:py-8');
+      expect(ar?.classList).toContain('lg:pr-0');
+      expect(ar?.classList).toContain('lg:pl-28');
+    });
+
+    it('should have gap 2rem column', () => {
+      const ar = area();
+      expect(ar?.classList).toContain('flex');
+      expect(ar?.classList).toContain('flex-col');
+      expect(ar?.classList).toContain('gap-8');
+    });
   });
 });
