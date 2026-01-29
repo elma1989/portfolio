22bd0f301fd25de8d8b808d786d849230b913d2d
Marco Elste <marco.elste@web.de>
2026-01-29 11:47:45 +0100

    chore: Updated LegalNoticeComponent - Added all paragraphs

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -25,7 +25,12 @@ type InprintParagraph = {
 export class LegalNoticeComponent {
   private sec: SectionService = inject(SectionService);
   private readonly ps: InprintParagraph[] = [
-    {id: 'aceptance', numberParagraphs: 1}
+    {id: 'acceptance', numberParagraphs: 1},
+    {id: 'ownership', numberParagraphs: 2},
+    {id: 'rights', numberParagraphs: 1},
+    {id: 'use', numberParagraphs: 1},
+    {id: 'disclaimer', numberParagraphs: 1},
+    {id: 'indemnity', numberParagraphs: 1}
   ]
 
   constructor() {
@@ -36,6 +41,11 @@ export class LegalNoticeComponent {
 
   get paragraphs(): InprintParagraph[] { return this.ps; }
 
+  /**
+   * Creates an array of numbers from 1 to maximum of paragraphs.
+   * @param value - maximum paragraphs.
+   * @returns - Counter-Array.
+   */
   getCounter(value: number): number[] {
     const numbers: number[] = [];
     for(let i = 1; i<= value; i++) {
