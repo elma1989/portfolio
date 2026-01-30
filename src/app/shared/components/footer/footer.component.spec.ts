929a6e034005728f740b54e2ebfebe958ad555d0
Marco Elste <marco.elste@web.de>
2026-01-30 09:37:52 +0100

    chore: Updated FooterComponent - Added content limiter

--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +14,51 @@ describe('FooterComponent', () => {
 
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Content', () => {
+    const content: () => HTMLDivElement | null = 
+      () => element.querySelector('.content');
+    
+    it('should have content limiter', () => {
+      expect(content()).toBeTruthy();
+    });
+
+    it('should have padding-x 1rem, bottom 2rem on mobile', () => {
+      const con = content();
+      expect(con?.classList).toContain('px-4');
+      expect(con?.classList).toContain('pb-8');
+    });
+
+    it('should have padding-x 7rem desktop', () => {
+      expect(content()?.classList).toContain('lg:px-28');
+    })
+    
+    it('should have gap 1 column on mobile', () => {
+      const con = content();
+      expect(con?.classList).toContain('flex');
+      expect(con?.classList).toContain('flex-col');
+      expect(con?.classList).toContain('gap-4');
+    });
+
+    it('should have x-center on mobile', () => {
+      expect(content()?.classList).toContain('items-center');
+    });
+
+    it('should have gap 0 row on desktop', () => {
+      const con = content();
+      expect(con?.classList).toContain('lg:flex-row');
+      expect(con?.classList).toContain('lg:gap-0');
+    });
+
+    it('should have x-between, y-center on desktop', () => {
+      expect(content()?.classList).toContain('lg:justify-between');
+    });
   });
 });
