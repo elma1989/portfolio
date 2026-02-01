e26fcdfd4744bd7b7d55bf69b4271d91989b229d
Marco Elste <marco.elste@web.de>
2026-02-01 10:31:50 +0100

    chore: Updated PrivacyPolicyCompoent - Added header, main, content-limiter

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { SectionService } from '../shared/services/section.service';
 import { SectionType } from '../shared/enums/section-type';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
 @Component({
   selector: 'privacy-policy',
-  imports: [],
+  imports: [
+    HeaderComponent
+  ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.css'
 })
