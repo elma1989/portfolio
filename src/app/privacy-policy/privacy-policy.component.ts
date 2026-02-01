2fe307a02d717b7a95828e5eee581cd6f314e52b
Marco Elste <marco.elste@web.de>
2026-02-01 13:06:10 +0100

    chore: Updated PrivacyPolicyComponent - Added structure data-protection, hosting

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { SectionService } from '../shared/services/section.service';
 import { SectionType } from '../shared/enums/section-type';
 import { HeaderComponent } from '../shared/components/header/header.component';
+import { TranslatePipe } from '../shared/pipes/translate.pipe';
 
 @Component({
   selector: 'privacy-policy',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    TranslatePipe
   ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.css'
