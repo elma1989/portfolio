ae53da24dd4311630b4f18f4199568b251d8667f
Marco Elste <marco.elste@web.de>
2026-02-03 18:18:57 +0100

    chore: Updated PrivacyPolicyComponent - Added footer

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -2,13 +2,15 @@ import { Component, computed, inject, Signal } from '@angular/core';
 import { SectionService } from '../shared/services/section.service';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { TranslatePipe } from '../shared/pipes/translate.pipe';
+import { FooterComponent } from "../shared/components/footer/footer.component";
 
 @Component({
   selector: 'privacy-policy',
   imports: [
     HeaderComponent,
-    TranslatePipe
-  ],
+    TranslatePipe,
+    FooterComponent
+],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.css'
 })
