370d6533820336d2079046e10bdc088eb71c4d82
Marco Elste <marco.elste@web.de>
2026-09-23 10:07:52 +0200

    fix: Contact-Submit
       - Upadated contact-section.component.ts:
          - Udated sendContactData() - Adjust fetch url for API

--- src/app/main-content/contact-section/contact-section.component.ts
+++ src/app/main-content/contact-section/contact-section.component.ts
@@ -156,7 +156,7 @@ export class ContactSectionComponent implements OnInit {
   private async sendContactData() {
     const {policy, ...data} = this.form.getRawValue();
     try {
-      const response = await fetch('/contact', {
+      const response = await fetch('/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
